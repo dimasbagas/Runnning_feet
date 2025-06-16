17e371fecab216533153cd630e6dcdd74e41423e
dimasbagas <danibaret014@gmail.com>
2025-06-16 19:47:44 +0700

    membuat tombol

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false, // Menghilangkan banner debug di pojok kanan atas
+      debugShowCheckedModeBanner:
+          false, // Menghilangkan banner debug di pojok kanan atas
       home: Firstpage(), // Menetapkan Firstpage sebagai halaman awal
     );
   }
@@ -26,33 +27,50 @@ class _FirstpageState extends State<Firstpage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        color: const Color(0xFF1FB69A), 
-        child: SafeArea( 
-          child: Center( 
-            child: Column( 
-              mainAxisAlignment: MainAxisAlignment.center, 
+        color: const Color(0xFF1FB69A),
+        child: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Image.asset(
-                  'assets/image/logo.png'
-                ),
-                const Text("RunningFeet",
-                style: TextStyle(
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xFFFE7F20)
-                ),),
-                const SizedBox(height: 10,),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 40.0),
-                child: Text(
-                  "Control your exercise activity tracking with various health statistics",
-                  textAlign: TextAlign.center,
+                Image.asset('assets/image/logo.png'),
+                const Text(
+                  "RunningFeet",
                   style: TextStyle(
-                    fontSize: 16,
-                    color: Color(0xffffffff)
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFFFE7F20),
                   ),
                 ),
-                )
+                const SizedBox(height: 10),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 40.0),
+                  child: Text(
+                    "Control your exercise activity tracking with various health statistics",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 16, color: Color(0xffffffff)),
+                  ),
+                ),
+                const SizedBox(height: 60),
+                ElevatedButton(onPressed: () {
+                  print("tombol ditekan");
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFFFE7F20),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 60, vertical: 10),
+                ),
+                child: Text(
+                  "Get Started",
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xffffffff)
+                  ),
+                  ),
+                ),
               ],
             ),
           ),
