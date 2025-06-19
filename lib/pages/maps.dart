21a3dc41fddc8276fb60f94be7b1849c234fb10c
dimasbagas <danibaret014@gmail.com>
2025-06-19 09:39:08 +0700

    menambahkan file maps

--- lib/pages/maps.dart
+++ lib/pages/maps.dart
@@ -8,8 +8,41 @@ class Maps extends StatefulWidget {
 }
 
 class _MapsState extends State<Maps> {
+  // MapLatLng loc = const MapLatLng(-7.160477, 113.481809);
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      // appBar: AppBar(
+      //   title: const Text("Maps Tracking"),
+      // ),
+      // body: SafeArea(
+      //   child: Column(
+      //     children: [
+      //       SizedBox(
+      //         height: 300,
+      //         child: SfMaps(
+      //           layers: [
+      //             MapTileLayer(
+      //               initialZoomLevel: 15,
+      //               initialFocalLatLng: loc,
+      //               urlTemplate:
+      //                   "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+      //               initialMarkersCount: 1,
+      //               markerBuilder: (BuildContext context, int index) {
+      //                 return MapMarker(
+      //                   latitude: loc.latitude,
+      //                   longitude: loc.longitude,
+      //                   child: const Icon(Icons.location_on, color: Colors.red),
+      //                 );
+      //               },
+      //             ),
+      //           ],
+      //         ),
+      //       ),
+      //     ],
+      //   ),
+      // ),
+    );
   }
 }
