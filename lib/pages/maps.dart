d00a82a4578fd94b8d45b23b3e1989dc73024659
dimasbagas <danibaret014@gmail.com>
2025-06-19 15:00:40 +0700

    memperbarui dependensi dan mengaktifkan fitur peta

--- lib/pages/maps.dart
+++ lib/pages/maps.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_maps/maps.dart';
 
 class Maps extends StatefulWidget {
   const Maps({super.key});
@@ -8,41 +9,41 @@ class Maps extends StatefulWidget {
 }
 
 class _MapsState extends State<Maps> {
-  // MapLatLng loc = const MapLatLng(-7.160477, 113.481809);
+  MapLatLng loc = const MapLatLng(-7.160477, 113.481809);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   title: const Text("Maps Tracking"),
-      // ),
-      // body: SafeArea(
-      //   child: Column(
-      //     children: [
-      //       SizedBox(
-      //         height: 300,
-      //         child: SfMaps(
-      //           layers: [
-      //             MapTileLayer(
-      //               initialZoomLevel: 15,
-      //               initialFocalLatLng: loc,
-      //               urlTemplate:
-      //                   "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
-      //               initialMarkersCount: 1,
-      //               markerBuilder: (BuildContext context, int index) {
-      //                 return MapMarker(
-      //                   latitude: loc.latitude,
-      //                   longitude: loc.longitude,
-      //                   child: const Icon(Icons.location_on, color: Colors.red),
-      //                 );
-      //               },
-      //             ),
-      //           ],
-      //         ),
-      //       ),
-      //     ],
-      //   ),
-      // ),
+      appBar: AppBar(
+        title: const Text("Maps Tracking"),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 300,
+              child: SfMaps(
+                layers: [
+                  MapTileLayer(
+                    initialZoomLevel: 15,
+                    initialFocalLatLng: loc,
+                    urlTemplate:
+                        "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+                    initialMarkersCount: 1,
+                    markerBuilder: (BuildContext context, int index) {
+                      return MapMarker(
+                        latitude: loc.latitude,
+                        longitude: loc.longitude,
+                        child: const Icon(Icons.location_on, color: Colors.red),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
